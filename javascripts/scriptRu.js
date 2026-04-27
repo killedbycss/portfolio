f91b6155825e1e15b7a15bce032edd5ae0de535c
tarasbalabaev <taras.balabaev@gmail.com>
2026-04-27 20:54:22 +0300

    fixed redirect

--- javascripts/scriptRu.js
+++ javascripts/scriptRu.js
@@ -11,23 +11,3 @@ function sendEmail() {
     window.location.href = `mailto:${email}?subject=${subject}&body=${body}`
   })
 }
-
-;(function () {
-  if (localStorage.getItem('language-set')) return
-
-  const userLang = (navigator.language || navigator.userLanguage)
-    .substring(0, 2)
-    .toLowerCase()
-  const pageLang = document.documentElement.lang.toLowerCase()
-
-  if (userLang === 'ru' && pageLang !== 'ru') {
-    window.location.href = '../indexRu.html'
-  } else if (userLang === 'en' && pageLang !== 'en') {
-    window.location.href = '../index.html'
-  }
-})()
-
-function manualSwitch(langPath) {
-  localStorage.setItem('language-set', 'true')
-  window.location.href = langPath
-}
